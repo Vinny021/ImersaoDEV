a233781e050dc321b502727e3dafc3b77a035e2e
Vinicius Barbosa <viniciusbarbosa021@gmail.com>
2021-04-11 20:52:11 -0300

    Teste de estrutura de cartas

---
+++ Aula07/script.js
@@ -0,0 +1,27 @@
+var carta1 = {
+    nome: "Seiya de Pégaso",
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+}
+
+var carta2 = {
+    nome: "Bulbasauro",
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+}
+
+var carta3 = {
+    nome: "Darth Vader",
+    ataque: 88,
+    defesa: 62, 
+    magia: 90
+}
+
+var carta4 = {
+    nome: "Shyvanna",
+    ataque: 85,
+    defesa: 80,
+    magia: 75
+}
